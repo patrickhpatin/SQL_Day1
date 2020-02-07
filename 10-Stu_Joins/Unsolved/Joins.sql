81a1dea3fd02ec01431b33e3f6309b42da0cb645
Patrick Patin <40468092+patrickhpatin@users.noreply.github.com>
2020-02-07 10:47:21 -0500

    Add files via upload

---
+++ 10-Stu_Joins/Unsolved/Joins.sql
@@ -0,0 +1,31 @@
+SELECT * FROM players;
+SELECT * FROM seasons_stats;
+
+
+SELECT players.id,
+	players.player,
+	players.height,
+	players.weight,
+	players.college,
+	players.born,
+	seasons_stats.position,
+	seasons_stats.tm
+FROM players
+	INNER JOIN seasons_stats ON players.id = seasons_stats.player_id
+WHERE players.id IN (0, 1, 2)
+	AND seasons_stats.tm IN ('INO', 'TOT');
+
+
+
+SELECT seasons_stats.player_id,
+	players.college,
+	seasons_stats.year,
+	seasons_stats.position,
+	seasons_stats.two_point_percentage,
+	seasons_stats.fg_percentage,
+	seasons_stats.ft_percentage,
+	seasons_stats.ts_percentage
+FROM seasons_stats
+	LEFT JOIN players ON players.id = seasons_stats.player_id
+WHERE players.id IN (0, 1, 2)
+	AND seasons_stats.tm IN ('INO', 'TOT');
